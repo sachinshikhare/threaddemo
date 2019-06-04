f57cda1a20c0eef4a7b0644f7002b15af93a1c6c
sah <sah@marketlogicsoftware.com>
2019-06-04 16:04:57 +0530

    Initial commit

---
+++ src/main/java/demo1/App.java
@@ -0,0 +1,39 @@
+package demo1;
+
+class RunnerThreadExtended extends Thread {
+
+    @Override
+    public void run() {
+        for (int cntr = 0; cntr < 10; cntr++) {
+            System.out.println("Thread: " +  Thread.currentThread().getName() + ", Counter: " + cntr);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class RunnerRunnableImplemented implements Runnable {
+
+    public void run() {
+        for (int cntr = 0; cntr < 10; cntr++) {
+            System.out.println("Thread: " +  Thread.currentThread().getName() + ", Counter: " + cntr);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        new RunnerThreadExtended().start();
+        new Thread(new RunnerRunnableImplemented()).start();
+        new RunnerThreadExtended().start();
+        new Thread(new RunnerRunnableImplemented()).start();
+    }
+}
